401e2094e72567c8c1b47ec9ebccadfc8271506c
Yufeng Xin <yxin@renci.org>
2020-11-16 16:33:20 -0500

    add a new app:  experiment profile

---
+++ profiles/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+# Create your models here.
+
+import uuid
+from enum import Enum
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+from accounts.models import AerpawUser
+from projects.models import Project
+from resources.models import ResourceStageChoice
+
+# Create your models here.
+
+class Experiment(models.Model):
+    uuid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    project = models.ForeignKey(
+        Project, related_name='experiment_of_project',null=True, on_delete=models.SET_NULL
+    )
+
+    profile = models.TextField()
+
+    created_by = models.ForeignKey(
+        AerpawUser, related_name='profile_created_by', null=True, on_delete=models.SET_NULL
+    )
+    created_date = models.DateTimeField(default=timezone.now)
+    
+    modified_by = models.ForeignKey(
+        AerpawUser, related_name='profile_modified_by', null=True, on_delete=models.SET_NULL
+    )
+    modified_date = models.DateTimeField(blank=True, null=True)
+
+    #reservations = models.ForeignKey(
+    #    'reservations.Reservation', related_name='experiment_of_reservation', null=True, on_delete=models.SET_NULL
+    #)
+
+    stage=models.CharField(
+      max_length=64,
+      choices=ResourceStageChoice.choices(),
+    )
+
+    class Meta:
+        verbose_name = 'AERPAW Profile'
+
+    def __str__(self):
+        return self.name
+
---
+++ profiles/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from django.conf.urls import include
+
+from reservations import views
+
+from .views import(
+    experiments,
+    experiment_create,
+    experiment_detail,
+    experiment_update,
+    experiment_delete
+)
+
+urlpatterns = [
+    path('', experiments, name='experiments'),
+    path('create', experiment_create, name='experiment_create'),
+    path('<uuid:experiment_uuid>', experiment_detail, name='experiment_detail'),
+    path('<uuid:experiment_uuid>/update', experiment_update, name='experiment_update'),
+    path('<uuid:experiment_uuid>/delete', experiment_delete, name='experiment_delete'),
+    path('', include(('reservations.urls', 'reservations'), namespace='reservations')),
+]
